986bf6e745977fae36ad6d370beab9d23bd27465
jiaj15 <jiaj15@outlook.com>
2020-03-06 02:12:27 -0800

    retreive q5

--- assign2_code/q5_train_atari_nature.py
+++ assign2_code/q5_train_atari_nature.py
@@ -29,44 +29,45 @@ address-ip-of-the-server:6006
 if __name__ == '__main__':
     # make env
     # config = config
-    # env = gym.make(config.env_name)
-    # env = MaxAndSkipEnv(env, skip=config.skip_frame)
-    # env = PreproWrapper(env, prepro=greyscale, shape=(80, 80, 1),
-    #                     overwrite_render=config.overwrite_render)
-    #
-    # # exploration strategy
-    # exp_schedule = LinearExploration(env, config.eps_begin,
-    #         config.eps_end, config.eps_nsteps)
-    #
-    # # learning rate schedule
-    # lr_schedule  = LinearSchedule(config.lr_begin, config.lr_end,
-    #         config.lr_nsteps)
-    #
-    # # train model
-    # model = NatureQN(env, config)
-    # model.run(exp_schedule, lr_schedule)
-    
-    # for test
-
     env = gym.make(config.env_name)
     env = MaxAndSkipEnv(env, skip=config.skip_frame)
     env = PreproWrapper(env, prepro=greyscale, shape=(80, 80, 1),
                         overwrite_render=config.overwrite_render)
-
-    model = NatureQN(env, config)
-    model.load(0, well_trained=True)
-
-    env = MaxAndSkipEnvForTest(env)
     
-    ob = env.reset()
-    for i in range(20):
-        ob = env.reset()
-        while True:
-                action = model.predict(ob)[0]
-                ob, r, done, info = env.step(action)
-                if done:
-                        break
-                #env.render()
+    # exploration strategy
+    exp_schedule = LinearExploration(env, config.eps_begin,
+            config.eps_end, config.eps_nsteps)
+    
+    # learning rate schedule
+    lr_schedule  = LinearSchedule(config.lr_begin, config.lr_end,
+            config.lr_nsteps)
+    
+    # train model
+    model = NatureQN(env, config)
+    model.run(exp_schedule, lr_schedule)
+    
+    # for test
+
+#     env = gym.make(config.env_name)
+#     env = MaxAndSkipEnv(env, skip=config.skip_frame)
+#     env = PreproWrapper(env, prepro=greyscale, shape=(80, 80, 1),
+#                         overwrite_render=config.overwrite_render)
+
+#     model = NatureQN(env, config)
+#     model.load(0, well_trained=True)
+
+#     env = MaxAndSkipEnvForTest(env)
+    
+#     ob = env.reset()
+#     for i in range(20):
+#         ob = env.reset()
+#         while True:
+#                 action = model.predict(ob)[0]
+#                 ob, r, done, info = env.step(action)
+#                 if done:
+#                         break
+#                 #env.render()
         
         
-    print(model.predict(ob))
+#     print(model.predict(ob))
+# # 
